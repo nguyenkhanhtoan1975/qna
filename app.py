6af7a9709bc0888235f996e6b801a432d3f304a5
Toan Nguyen <nktoan.ai@gmail.com>
2024-07-15 23:31:29 +0700

    The second commit

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 from langchain_community.document_loaders import JSONLoader
 from langchain.embeddings.sentence_transformer import SentenceTransformerEmbeddings
 from langchain_community.vectorstores import Chroma
